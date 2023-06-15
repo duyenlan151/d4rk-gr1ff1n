88f67268f946a1330381b4e321d5f8352c5757a0
Jack Miller <jackmiller0899@gmail.com>
2023-06-15 12:29:47 +0700

    Minor update

--- src/root.routing.tsx
+++ src/root.routing.tsx
@@ -5,13 +5,15 @@ import { lazy } from "react";
 import { routes as adminRoutes } from "./modules/admin/admin.routing.tsx";
 import { routes as appRoutes } from "./modules/app/app.routing.tsx";
 
-
+// Guards
 import composedGuard from "./shared/guards/composed.guard.ts";
 import authGuard from "./shared/guards/auth.guard.ts";
 import noAuthGuard from "./shared/guards/no-auth.guard.ts";
+
+// Eager components
 import App from "./modules/app/app.component.tsx";
 
-
+// Lazy components
 const Login = lazy(() => import("./modules/auth/login/login.component"));
 const SignUp = lazy(() => import("./modules/auth/sign-up/sign-up.component.tsx"));
 const Admin = lazy(() => import("./modules/admin/admin.component.tsx"));
